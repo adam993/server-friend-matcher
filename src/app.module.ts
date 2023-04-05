ac0f8158849b07ea96c0b83d9943b9c80c22e15a
Adam Radivojevic <adamradivojevic@gmail.com>
2023-04-05 16:01:32 +0200

    Feat: TypeORM, env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { ApiModule } from './api/api.module';
+import { AuthModule } from './auth/auth.module';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    ApiModule, AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
